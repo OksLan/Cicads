70471c7de3e9ea26585f9ab10ad335de06838092
Oksana Lanovenko <158132001+OksLan@users.noreply.github.com>
2024-10-06 19:00:16 +0400

    Add files via upload

---
+++ tests.js
@@ -0,0 +1,15 @@
+const initYear = 2024;
+const period1 = 13;
+const period2 = 17;
+
+let year = initYear;
+
+// Цикл продолжается до тех пор, пока не найдём следующий год, когда оба события совпадут
+while (true) {
+    if ((year - initYear) % period1 === 0 && (year - initYear) % period2 === 0) {
+        break;
+    }
+    year++;
+}
+
+console.log(`Следующее совпадение событий произойдет в ${year} году.`);
